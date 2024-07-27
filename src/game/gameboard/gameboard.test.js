5287506de47b859e2bd1291b678d64b240218f1d
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2024-07-27 15:19:38 +0500

    Tests that determine whether or not ship placed at specific coordinates

--- src/game/gameboard/gameboard.test.js
+++ src/game/gameboard/gameboard.test.js
@@ -1,3 +1,4 @@
+import Ship from "../ship/ship";
 import Gameboard from "./gameboard";
 
 describe('Testing Gameboard Class', () => {
@@ -6,15 +7,35 @@ describe('Testing Gameboard Class', () => {
         const board = new Array(10).fill([...(new Array(10).fill([]))]);
         expect(gameboard.getGameBoard()).toEqual(board);
     });
-    test('Place ship at specific coordinates', () => {
-        
+
+    test('Place ship at specific coordinates: Case 1', () => {
+        const gameboard = new Gameboard();
+        const carrier = new Ship(5);
+        gameboard.placeShip(carrier, [4, 4]);
+        const board = new Array(10).fill([...(new Array(10).fill([]))]);
+        const positions = [[4, 4], [5, 4], [6, 4], [7, 4], [8, 4]];
+        positions.forEach(position => {
+            const x = position[0];
+            const y = position[1];
+            board[x][y] = carrier;
+        })
+        expect(gameboard.getGameBoard()).toEqual(board)
     });
+    
+    test('Place ship at specific coordinates: Case 2', () => {
+        const gameboard = new Gameboard();
+        const carrier = new Ship(5);
+        expect(gameboard.placeShip(carrier, [6, 7])).toBe("ERROR")
+    });
+
     test('Whether or not attack hit a ship', () => {
 
     });
+
     test('Keep tracking of missed attacks properly', () => {
 
     });
+    
     test('Report whether or not all of their ships have been sunk', () => {
 
     })
