95e3b42e1d72cdcb13f2311940d965a19933ffa7
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2024-08-03 21:41:26 +0500

    Add ships for selection panel

--- src/ui/game/selectionPanel.js
+++ src/ui/game/selectionPanel.js
@@ -5,6 +5,14 @@ import { loadImage } from "../utils/loadImage";
 import Start from '../../assets/start.png'
 import Notice from '../../assets/notice.png'
 
+import Battleship from '../../assets/ships/battleship.png';
+import Cruiser from '../../assets/ships/cruiser.png';
+import Destroyer from '../../assets/ships/destroyer.png';
+import Rescue from '../../assets/ships/rescue.png';
+import Patrol from '../../assets/ships/patrol.png';
+
+import createShip from "./ship";
+
 function createSelectionPanel() {
     const selection = createElement('div', 'selection');
     
@@ -20,6 +28,18 @@ function createSelectionPanel() {
 
     appearElement(notice, noticeImage);
     appearElement(selection, notice);
+
+    const battleship = createShip('battleship', Battleship);
+    const cruiser = createShip('cruiser', Cruiser);
+    const destroyer = createShip('destroyer', Destroyer);
+    const rescue = createShip('rescue', Rescue);
+    const patrol = createShip('patrol', Patrol);
+
+    appearElement(selection, battleship);
+    appearElement(selection, cruiser);
+    appearElement(selection, destroyer);
+    appearElement(selection, rescue);
+    appearElement(selection, patrol);
 
     return selection;
 }
