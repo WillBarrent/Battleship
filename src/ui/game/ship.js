e6d50582a7329a2bfbcb2643005e8c39bbf30bc3
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2024-08-04 22:32:22 +0500

    Create LoadShip function that palce ship at certain position

--- src/ui/game/ship.js
+++ src/ui/game/ship.js
@@ -1,6 +1,12 @@
 import { appearElement } from "../utils/appearElement"
 import { createElement } from "../utils/createElement"
-import { loadImage } from "../utils/loadImage"
+import { loadImage } from "../utils/loadImage";
+
+import Battleship from '../../assets/ships/battleship.png';
+import Cruiser from '../../assets/ships/cruiser.png';
+import Destroyer from '../../assets/ships/destroyer.png';
+import Rescue from '../../assets/ships/rescue.png';
+import Patrol from '../../assets/ships/patrol.png';
 
 // Battleship
 // Cruiser
@@ -17,4 +23,32 @@ function createShip(shipName, image) {
     return ship;
 }
 
-export { createShip as default } 
+function loadShips(board) {
+    const shipPositions = board.getShipPositions();
+    const gameboard = board.getGameBoard();
+
+    const shipsAssets = {
+        'battleship': Battleship,
+        'cruiser': Cruiser,
+        'destroyer': Destroyer,
+        'rescue': Rescue,
+        'patrol': Patrol,
+    };
+
+    const boardClass = document.querySelector('.board');
+
+    let x = null, y = null;
+    let start = 2;
+
+    shipPositions.forEach((position) => {
+        x = position[0], y = position[1];
+        const ship = gameboard[x][y];
+        const shipType = ship.type;
+        const buildShip = createShip(shipType, shipsAssets[shipType]);
+        buildShip.style.gridRow = `${start + x} / ${start + x + ship.length}`;
+        buildShip.style.gridColumn = `${start + y} / ${start + y + 1}`;
+        appearElement(boardClass, buildShip);
+    });
+}
+
+export { createShip as default, loadShips } 
