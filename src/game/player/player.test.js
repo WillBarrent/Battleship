ab185417b17afd09ec4e208444ef8d67b1a0c189
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2024-08-09 22:14:10 +0500

    Testing random gameboard generation function for any issue

--- src/game/player/player.test.js
+++ src/game/player/player.test.js
@@ -1,3 +1,4 @@
+import Ship from "../ship/ship";
 import Player from "./player";
 
 describe('Testing Player Class', () => {
@@ -14,7 +15,52 @@ describe('Testing Player Class', () => {
         const testAIPlayerGameboard = testAIPlayer.gameboard;
 
         const [attacked, X, Y] = testAIPlayer.makeRandomAttack(testPlayerGameboard);
-        
+
         expect(testPlayerGameboard.getGameBoard()[X][Y]).toBe(attacked);
+    })
+
+    test('Random gameboad generation', () => {
+        const shipTypes = [
+            {
+                name: "battleship",
+                length: 5,
+            },
+            {
+                name: "cruiser",
+                length: 4,
+            },
+            {
+                name: "destroyer",
+                length: 4,
+            },
+            {
+                name: "rescue",
+                length: 3,
+            },
+            {
+                name: "patrol",
+                length: 3,
+            }
+        ];
+
+        const ai = new Player();
+
+        ai.randomGameboard();
+
+        const gameboard = ai.gameboard.getGameBoard();
+        const allShipPositions = ai.gameboard.getShipPositions();
+
+        let isAllCorrect = true;
+
+        for (let i = 0; i < shipTypes.length && isAllCorrect; i++) {
+            const type = shipTypes[i];
+            const ship = new Ship(type.length, type.name);
+
+            const x = allShipPositions[i][0];
+            const y = allShipPositions[i][1];
+            isAllCorrect = !((x + ship.length - 1) > 9) && ship.type === type.name;
+        }
+
+        expect(isAllCorrect).toBe(true);
     })
 })
